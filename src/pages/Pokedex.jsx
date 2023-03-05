907d2b4039c14a818ad170e666c898653f68ebe3
daes021@hotmail.com <daes021@hotmail.com>
2023-03-05 01:20:26 -0500

    2da version

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -9,6 +9,7 @@ const Pokedex = () => {
     const [pokemonsFilter, setPokemonsFilter] = useState([])
     const [types, setTypes] = useState([])
     const [namePokemon, setNamePokemon] = useState("")
+    const [pokemonType, setPokemonType] = useState("")
 
     const nameTrainer = useSelector(state => state.nameTrainer)
 
@@ -18,12 +19,24 @@ const Pokedex = () => {
       setNamePokemon(name)
     }
 
+    const handleChangeSelect = (e) => {
+      setPokemonType(e.target.value)
+    }
+
     useEffect(() => {
-        const URL = "https://pokeapi.co/api/v2/pokemon/?limit=30"
+        const URL = `https://pokeapi.co/api/v2/${pokemonType ? `type/${pokemonType}/` : "pokemon/?limit=20"}`
+        console.log(URL)
         axios.get(URL)
-            .then(res => setPokemons(res.data.results))
+            .then(res => {
+                if(pokemonType){
+                  const newPokemons = res.data.pokemon.map(pokemon => pokemon.pokemon)
+                  setPokemons(newPokemons)
+                }else {
+                  setPokemons(res.data.results)
+                }
+              })
             .catch(err => console.log(err))
-    }, [])
+    }, [pokemonType])
 
     useEffect(() => {
       const URL = "https://pokeapi.co/api/v2/type/"
@@ -33,24 +46,27 @@ const Pokedex = () => {
     }, [])
 
     useEffect(() => {
-      const newPokemons = pokemons.filter(pokemon => pokemon.name.includes(namePokemon))
-      setPokemonsFilter(newPokemons)
-    }, [namePokemon])
+      const newPokemons = pokemons.filter(pokemon => pokemon.name.
+        includes(namePokemon))
+        setPokemonsFilter(newPokemons)
+    }, [namePokemon, pokemons])
 
   return (
     <main>
-        <header>
+        <header className='pokedex_header'>
             <h1>Pokedex</h1>
             <p>Welcome <span>{nameTrainer}</span>, here you can find your favorite pokemon</p>
             <form onSubmit={handleSubmit} classname='pokedex_form'>
               <div className='pokedex_search'>
-                <input type="text" id='namePokemon' />
-                <button type='submit'>Search</button>
+                <input className='pokedex_input' type="text" id='namePokemon' />
+                <button className='pokedex_btn' type='submit'>Search</button>
               </div>
-              <select className='pokedex_select'>
+              <select onChange={handleChangeSelect}
+              className='pokedex_select'>
                 <option value="">All pokemons</option>
               {
-                types.map(type => <option value={type.name} key={type.url}>{type.name}</option>)
+                types.map(type => <option value={type.name} key=
+                {type.url}>{type.name}</option>)
               }
               </select>
             </form>
